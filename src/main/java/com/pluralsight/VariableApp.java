d7485cbb9e73d93788fa6a6924629e3c5b2c2faa
sgrinley <shamar808@yahoo.com>
2026-04-09 10:38:22 -0700

    feat: Establish variables first

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -3,5 +3,15 @@ package com.pluralsight;
 public class VariableApp {
     public static void main(String[] args) {
 
+        String favoriteColor = "Blue";
+        int startYear = 2026;
+        Char middleInitial = "A";
+        boolean hasPets = "False";
+        String message = "Walk by faith not by sight.";
+
+
+
+
+
     }
 }
