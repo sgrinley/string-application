a388ae0d5d0f5e346d1d1b24e73e115ddd923463
sgrinley <shamar808@yahoo.com>
2026-04-09 10:58:58 -0700

    feat: Print main class

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -3,12 +3,26 @@ package com.pluralsight;
 public class VariableApp {
     public static void main(String[] args) {
 
+        // Question 1
         String favoriteColor = "Blue";
-        int startYear = 2026;
-        Char middleInitial = "A";
-        boolean hasPets = "False";
+
+        // Question 2
+        int startYearUp = 2026;
+
+        // Question 3
+        char middleInitial = 'A';
+
+        // Question 4
+        boolean hasPets = false;
+
+        // Question 5
         String message = "Walk by faith not by sight.";
 
+        System.out.println("My favorite color is " + favoriteColor);
+        System.out.println("I started this program in " + startYearUp);
+        System.out.println("My middle initial is " + middleInitial);
+        System.out.println("Do I have pets? " + false);
+        System.out.println(message);
 
 
 
