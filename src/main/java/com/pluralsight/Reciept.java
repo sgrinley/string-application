cedac4950b62f9fb20b2289331638d61bc931b5c
sgrinley <shamar808@yahoo.com>
2026-04-09 11:44:53 -0700

    Initial Commit

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+//      Step 2: Question 2
+
+        int daysInWeek = 7;
+        double coffeePrice = 4.99;
+        char favoriteLetter = 'G';
+        boolean isRaining = false;
+
+        System.out.println("There are " + daysInWeek + "days in a week.");
+        System.out.println("Coffee cost $" + "coffeePrice");
+        System.out.println("My favorite letter is " + favoriteLetter);
+        System.out.println("Is it raining? " + isRaining);
+
+
+
+    }
+}
