e5c4b1918ed749939f2a06298a2335f138107a89
sgrinley <shamar808@yahoo.com>
2026-04-09 11:49:40 -0700

    feat: Display Receipt variables first

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+
+        // Step 2: Question 2
+        int daysInWeek = 7;
+
+        // Step 2: Question 3
+        double coffeePrice = 4.99;
+
+        // Step 2: Question 4
+        char favoriteLetter = 'G';
+
+        // Step 2: Question 5
+        boolean isRaining = false;
+
+
+
+
+    }
+}
